712698a4eeb49815f2aa83d2887bc3f1582b0ce5
Viviane Sousa de Melo <vivianemeloscc@gmail.com>
2023-04-28 18:31:07 -0300

    Adicionando o arquivo contato

---
+++ contato.py
@@ -0,0 +1,8 @@
+class Contato:
+    def __init__(self, nome, sobrenome, telefone, data_de_nascimento, email):
+        self.nome = nome
+        self.sobrenome = sobrenome 
+        self.telefone = telefone 
+        self.data_de_nascimento = data_de_nascimento 
+        self.email = email 
+        self.proximo = None
