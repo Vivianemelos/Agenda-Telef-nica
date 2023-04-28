715dcd20ca7a2a0f7055c82ae7c6ea158d6c23fc
Viviane Sousa de Melo <vivianemeloscc@gmail.com>
2023-04-28 18:35:48 -0300

    Adicionando o arquivo main

---
+++ main.py
@@ -0,0 +1,44 @@
+from agenda import Agenda
+
+def main():
+    lista = Agenda(nome=None, sobrenome=None, telefone=None, data_de_nascimento=None, email=None)
+    while True:
+        print("Escolha uma opção:")
+        print("1. Adicionar contato")
+        print("2. Visualizar contato")
+        print("3. Atualizar contato")
+        print("4. Sair")
+        
+        opcao = input("Opção: ")
+        if opcao == "1":
+            nome = input("Nome: ")
+            sobrenome = input("Sobrenome: ")
+            telefone = input("Telefone: ")
+            nascimento = input("Data de Nascimento: ")
+            email = input("Email: ")
+            lista.adicionar(nome, sobrenome, telefone, nascimento, email)
+            print("Contato adicionado com sucesso!")
+        
+        elif opcao == "2":
+            lista.visualizar()
+        
+        elif opcao == "3":
+            nome = input("Nome: ")
+            telefone = input("Telefone: ")
+            email = input("Email: ")         
+            lista.atualizar(nome, telefone, email)    
+        
+        elif opcao =="4": 
+            print("4. Sair") 
+            break   
+        
+        else:
+            print("Opção inválida!")
+            
+        
+        
+    
+if __name__== "__main__":
+    main()
+    
+    
